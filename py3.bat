06543aed862561e71bbc141015380f22a28d9a48
kderlin <kderlin@10b87387-f67f-2b5a-19f4-7aff8575588b>
2011-01-03 05:27:09 +0000

    改进打包脚本

--- py3.bat
+++ py3.bat
@@ -1,7 +1,7 @@
 @echo off
 REM echo %~dp0
 cd /d %~dp0
-pause
-for /f "tokens=4 delims= " %%i in ('findstr "$Revision:" lzuauto3\main.py') do set j=%%i
-REM echo %j%
-7z a -xr!*.svn* -xr!*.pyc lzuauto-1.1.3.%j%-py3-src.7z lzuauto3
+for /f "tokens=2 delims='" %%i in ('findstr /r "'[0-9]\.[0-9]\.[0-9]'" lzuauto3\main.py') do set ver=%%i
+for /f "tokens=4 delims= " %%i in ('findstr "$Revision:" lzuauto3\main.py') do set rev=%%i
+echo %ver%.%rev%
+7z a -xr!*.svn* -xr!*.pyc -xr!*__pycache__* lzuauto-%ver%.%rev%-py3-src.7z lzuauto3
